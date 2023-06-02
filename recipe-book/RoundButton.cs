e232eec2081b7d34daea5d046375ac7f69698952
Scorpi-ON <s.scorpi-on@ya.ru>
2023-06-03 01:35:37 +0300

    Добавил класс для круглой кнопки (требует модификации до закруглённой)

---
+++ recipe-book/RoundButton.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Drawing.Drawing2D;
+
+namespace recipe_book
+{
+    internal class RoundButton : Panel
+    {
+        private readonly Button button;
+
+        [Browsable(true)]
+        public override string Text
+        {
+            get => button.Text;
+            set => button.Text = value;
+        }
+
+        public RoundButton()
+        {
+            button = new Button() { Dock = DockStyle.Fill };
+            Controls.Add(button);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            using GraphicsPath graphicsPath = new();
+            graphicsPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            Region = new Region(graphicsPath);
+            base.OnPaint(e);
+        }
+    }
+}
