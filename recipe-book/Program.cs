57a8f72f231affd74e8dd4bba21989dfcb0eaff7
Scorpi-ON <s.scorpi-on@ya.ru>
2023-06-02 19:48:21 +0300

    MainForm теперь снова главная форма

--- recipe-book/Program.cs
+++ recipe-book/Program.cs
@@ -11,7 +11,7 @@ namespace recipe_book
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AuthForm());
+            Application.Run(new MainForm());
         }
     }
 }
