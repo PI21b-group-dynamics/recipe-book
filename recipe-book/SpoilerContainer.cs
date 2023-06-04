0132d2cc3bc362c07889b0379db399afdbddf029
Scorpi-ON <s.scorpi-on@ya.ru>
2023-06-04 21:01:51 +0300

    Добавил виджет-спойлер

---
+++ recipe-book/SpoilerContainer.cs
@@ -0,0 +1,34 @@
+﻿namespace recipe_book
+{
+    internal class SpoilerContainer : TableLayoutPanel
+    {
+        private string? _title;
+        public string Title
+        {
+            get => _title ?? "Не задано";
+            set => _title = value;
+        }
+
+        private readonly Button btnSpoiler;
+
+        public SpoilerContainer()
+        {
+            btnSpoiler = new()
+            {
+                Text = Title,
+                TextAlign = ContentAlignment.MiddleLeft,
+                AutoSize = true,
+                AutoSizeMode = AutoSizeMode.GrowAndShrink,
+                Dock = DockStyle.Fill
+            };
+            btnSpoiler.Click += btnSpoiler_Click;
+            Controls.Add(btnSpoiler);
+        }
+
+        private void btnSpoiler_Click(object? sender, EventArgs e)
+        {
+            Control contentControl = GetControlFromPosition(0, 1) ?? throw new NullReferenceException();
+            contentControl.Visible = !contentControl.Visible;
+        }
+    }
+}
