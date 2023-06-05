ddf31dd7cc31a21e5e4adc464e6efa46b195f453
Scorpi-ON <s.scorpi-on@ya.ru>
2023-06-05 14:38:34 +0300

    Набросал макет формы, пока без нейминга

--- recipe-book/EditProfileForm.Designer.cs
+++ recipe-book/EditProfileForm.Designer.cs
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AccountEditForm";
+            tableLayoutPanel1 = new TableLayoutPanel();
+            txtPassword = new TextBox();
+            txtEmail = new TextBox();
+            txtLogin = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(txtPassword, 0, 3);
+            tableLayoutPanel1.Controls.Add(txtEmail, 0, 2);
+            tableLayoutPanel1.Controls.Add(txtLogin, 0, 1);
+            tableLayoutPanel1.Controls.Add(button1, 0, 4);
+            tableLayoutPanel1.Controls.Add(button2, 1, 4);
+            tableLayoutPanel1.Controls.Add(button3, 0, 6);
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.Padding = new Padding(10, 11, 10, 11);
+            tableLayoutPanel1.RowCount = 7;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 118F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.Size = new Size(347, 309);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            tableLayoutPanel1.SetColumnSpan(txtPassword, 2);
+            txtPassword.Dock = DockStyle.Top;
+            txtPassword.Location = new Point(13, 198);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PlaceholderText = "Пароль";
+            txtPassword.Size = new Size(321, 27);
+            txtPassword.TabIndex = 1;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtEmail
+            // 
+            tableLayoutPanel1.SetColumnSpan(txtEmail, 2);
+            txtEmail.Dock = DockStyle.Top;
+            txtEmail.Location = new Point(13, 165);
+            txtEmail.Name = "txtEmail";
+            txtEmail.PlaceholderText = "E-mail";
+            txtEmail.Size = new Size(321, 27);
+            txtEmail.TabIndex = 0;
+            // 
+            // txtLogin
+            // 
+            tableLayoutPanel1.SetColumnSpan(txtLogin, 2);
+            txtLogin.Dock = DockStyle.Top;
+            txtLogin.Location = new Point(13, 132);
+            txtLogin.Name = "txtLogin";
+            txtLogin.PlaceholderText = "Логин";
+            txtLogin.Size = new Size(321, 27);
+            txtLogin.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(13, 231);
+            button1.Name = "button1";
+            button1.Size = new Size(157, 29);
+            button1.TabIndex = 7;
+            button1.Text = "Отмена";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Fill;
+            button2.Location = new Point(176, 231);
+            button2.Name = "button2";
+            button2.Size = new Size(158, 29);
+            button2.TabIndex = 8;
+            button2.Text = "Сохранить";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.AutoSize = true;
+            button3.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            tableLayoutPanel1.SetColumnSpan(button3, 2);
+            button3.Dock = DockStyle.Top;
+            button3.Location = new Point(13, 266);
+            button3.Name = "button3";
+            button3.Size = new Size(321, 30);
+            button3.TabIndex = 9;
+            button3.Text = "Удалить профиль";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            tableLayoutPanel1.SetColumnSpan(pictureBox1, 2);
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(13, 14);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(321, 112);
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // EditProfileForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(347, 309);
+            Controls.Add(tableLayoutPanel1);
+            Name = "EditProfileForm";
+            Text = "Редактирование профиля";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox txtPassword;
+        private TextBox txtEmail;
+        private TextBox txtLogin;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private PictureBox pictureBox1;
     }
 }
