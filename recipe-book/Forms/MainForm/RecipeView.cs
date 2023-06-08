317bcb31f868cf4d409b8c715e4a565bc76c3ec7
Scorpi-ON <s.scorpi-on@ya.ru>
2023-06-08 20:08:18 +0300

    Добавил функцию клика на кнопку редактирования рецепта

--- recipe-book/Forms/MainForm/RecipeView.cs
+++ recipe-book/Forms/MainForm/RecipeView.cs
@@ -7,5 +7,11 @@
             tbcMainFormTabs.SelectedTab = tabListOfRecipes;
             _previousSelectedTab = tabRecipeView;
         }
+
+        private void btnEditRecipe_Click(object sender, EventArgs e)
+        {
+            tbcMainFormTabs.SelectedTab = tabCreateOrEditRecipe;
+            _previousSelectedTab = tabRecipeView;
+        }
     }
 }
